e64ff82af04e22150632cd232a16a6f051e5efea
Steven Campos <steven.dev7599@gmail.com>
2022-10-03 10:05:51 -0600

    Se mejoro el metodo para calcular el tamaño de la pantalla

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 
+
+let h1 = document.documentElement.scrollHeight;
+let h2 = document.documentElement.clientHeight;
+let scrollUnit = (h1 - h2) /100;
+const rootStyles = document.documentElement.style;
+
+addEventListener("resize",() => {
+    h1 = document.documentElement.scrollHeight;
+    h2 = document.documentElement.clientHeight;
+    scrollUnit = (h1 - h2) /100;
+})
+
 //3620
 addEventListener('scroll', () => {
-    const h1 = document.documentElement.scrollHeight;
-    const h2 = document.documentElement.clientHeight;
-    const scrollUnit = (h1 - h2) /100;
-    const rootStyles = document.documentElement.style;
     rootStyles.setProperty('--width', Math.round(scrollY/scrollUnit));
 });
